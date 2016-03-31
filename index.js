5f95be89102af39961177358211660891d090c50
ajordon <airjordon2@gmail.com>
2016-03-30 22:35:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
+
+// load manifests
+// scripts
+require('./assets/scripts/index.js');
+require('./assets/scripts/scripts.js');
+
+// styles
+require('./assets/styles/index.scss');
+
+// attach jQuery globally
+require('expose?$!jquery');
+require('expose?jQuery!jquery');
